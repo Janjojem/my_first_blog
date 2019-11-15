62203cc000cbdd2a756022d01fb96405cf6a2bdb
unknown <mert.sertbas@gmail.com>
2019-11-15 13:53:00 +0300

    optimize

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 
+
 # Create your views here.
 
 Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
